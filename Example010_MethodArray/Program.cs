5af557e1063a01bef7ab61f0cefb037a6c193118
alex0210 <a0210@yandex.ru>
2023-02-14 01:04:10 +0300

    Added MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 1, 12, 31, 4, 18, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index +1; на каждом этапе нам нужно будет увеличивать значение индекса
+    index++; // более короткая форма увеличения индекса
+}
